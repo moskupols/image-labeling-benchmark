de9776165e07c768172a76b37e2b1ef384341972
Feodor Alexeev <feodor.alexeev@gmail.com>
2015-07-30 12:14:33 +0500

    Add main

---
+++ main.cxx
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+#include "counter.h"
+#include "matrix.h"
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> v(n, vector<int>(m, 0));
+    for (auto &r : v)
+    {
+        string s;
+        cin >> s;
+        for (size_t i = 0; i < s.size(); ++i)
+            if (s[i] == '1')
+                r[i] = 1;
+    }
+
+    Matrix wrapped = Matrix::wrapVectors(v);
+    int ans = getNumberOfAllLevelComponentsBinary(wrapped);
+
+    cout << ans << endl;
+
+    return 0;
+}
+
