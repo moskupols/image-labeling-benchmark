243bbb07ff99aa176562c83e788688da0bf6b7da
Feodor Alexeev <feodor.alexeev@gmail.com>
2015-08-23 02:56:51 +0500

    Add matrix constructor from range

--- src/matrix.hxx
+++ src/matrix.hxx
@@ -2,6 +2,7 @@
 #define MATRIX_HXX
 
 #include <vector>
+#include <cassert>
 
 using std::vector;
 
@@ -14,6 +15,16 @@ public:
     VectorMatrix(int rows, int cols, int value = 0);
     explicit VectorMatrix(const Vectors &data): data(data) {}
     explicit VectorMatrix(Vectors &&data): data(data) {}
+
+    template<class InputIt>
+    VectorMatrix(int rows, int cols, InputIt begin, InputIt end):
+        data(rows, vector<int>(cols))
+    {
+        for (auto& row : data)
+            for (int& cell : row)
+                cell = *begin++;
+        assert(begin == end);
+    }
 
     int getMatrixWidth() const { return data[0].size(); }
     int getMatrixHeight() const { return data.size(); }
@@ -32,6 +43,16 @@ public:
     IntArrayMatrix(): data(nullptr) {}
     IntArrayMatrix(std::size_t rows, std::size_t cols, int value = 0);
     explicit IntArrayMatrix(const Vectors &data);
+
+    template<class InputIt>
+    IntArrayMatrix(std::size_t rows, std::size_t cols, InputIt begin, InputIt end):
+        height(rows),
+        width(cols),
+        data(new int[rows * cols])
+    {
+        int* dataEnd = std::copy(begin, end, data);
+        assert(dataEnd  == data + rows * cols);
+    }
 
     ~IntArrayMatrix() { delete[] data; }
 
